2f7777e0a01e9a14bcaa5a07ec70e3750c91d887
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-02-12 11:27:07 +0530

    Add loader to the home page and also display posts using API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,56 +1,58 @@
 import styles from '../styles/home.module.css';
 
-const Home = () => {
+const Home = ({posts}) => {
   return (
-    <div className={styles.postsList}>
-      <div className={styles.postWrapper}>
-        <div className={styles.postHeader}>
-          <div className={styles.postAvatar}>
-            <img
-                src="https://cdn-icons.flaticon.com/png/128/2202/premium/2202112.png?token=exp=1644591071~hmac=53c882810ea6b302934838d8fb7af074"
-                alt="user-pic"
-            />
-            <div>
-              <span className={styles.postAuthor}>Sarthak</span>
-              <span className={styles.postTime}>a minute ago</span>
-            </div>
-          </div>
-          <div className={styles.postContent}>Post Content</div>
+      <div className={styles.postsList}>
+        { posts.map ((post) => (
+            <div className={styles.postWrapper}>
+                <div className={styles.postHeader}>
+                <div className={styles.postAvatar}>
+                    <img
+                        src="https://cdn-icons.flaticon.com/png/128/2202/premium/2202112.png?token=exp=1644591071~hmac=53c882810ea6b302934838d8fb7af074"
+                        alt="user-pic"
+                    />
+                    <div>
+                    <span className={styles.postAuthor}>{post.user.name}</span>
+                    <span className={styles.postTime}>a minute ago</span>
+                    </div>
+                </div>
+                <div className={styles.postContent}>{post.content}</div>
 
-          <div className={styles.postActions}>
-            <div className={styles.postLike}>
-              <img
-                src="https://cdn-icons-png.flaticon.com/128/889/889140.png"
-                alt="likes-icon"
-              />
-              <span>5</span>
-            </div>
+                <div className={styles.postActions}>
+                    <div className={styles.postLike}>
+                    <img
+                        src="https://cdn-icons-png.flaticon.com/128/889/889140.png"
+                        alt="likes-icon"
+                    />
+                    <span>5</span>
+                    </div>
 
-            <div className={styles.postCommentsIcon}>
-              <img
-                src="https://cdn-icons-png.flaticon.com/128/2190/2190552.png"
-                alt="comments-icon"
-              />
-              <span>2</span>
-            </div>
-          </div>
-          <div className={styles.postCommentBox}>
-            <input placeholder="Start typing a comment" />
-          </div>
+                    <div className={styles.postCommentsIcon}>
+                    <img
+                        src="https://cdn-icons-png.flaticon.com/128/2190/2190552.png"
+                        alt="comments-icon"
+                    />
+                    <span>2</span>
+                    </div>
+                </div>
+                <div className={styles.postCommentBox}>
+                    <input placeholder="Start typing a comment" />
+                </div>
 
-          <div className={styles.postCommentsList}>
-            <div className={styles.postCommentsItem}>
-              <div className={styles.postCommentHeader}>
-                <span className={styles.postCommentAuthor}>Bill</span>
-                <span className={styles.postCommentTime}>a minute ago</span>
-                <span className={styles.postCommentLikes}>22</span>
-              </div>
+                <div className={styles.postCommentsList}>
+                    <div className={styles.postCommentsItem}>
+                    <div className={styles.postCommentHeader}>
+                        <span className={styles.postCommentAuthor}>Bill</span>
+                        <span className={styles.postCommentTime}>a minute ago</span>
+                        <span className={styles.postCommentLikes}>22</span>
+                    </div>
 
-              <div className={styles.postCommentContent}>Random comment</div>
+                    <div className={styles.postCommentContent}>Random comment</div>
+                    </div>
+                </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
+        ))}
     </div>
   );
 };
