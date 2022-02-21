ca9a49f9ec70d5d80e2e016f2b541363b7c150f6
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-02-21 11:39:23 +0530

    Make PrivateRoute function to remove the bug of settings page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import { Home, Login, SignUp, Settings } from '../pages';
 import { Loader, NavBar } from './';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from '../hooks';
+
+function PrivateRoute({ children }) {
+  const auth = useAuth();
+  
+ return auth.user ? children : <Navigate to="/login" />; 
+}
 
 const Page404 = () => {
   return <h1>404</h1>;
@@ -21,7 +27,14 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
-        <Route path="/settings" element={<Settings />}></Route>
+        <Route
+          path="/settings"
+          element={
+            <PrivateRoute>
+              <Settings />
+            </PrivateRoute>
+          }
+        ></Route>
 
         {/* When no route matches we use path="*" which means that this will be rendered */}
         <Route path="*" element={<Page404 />}></Route>
